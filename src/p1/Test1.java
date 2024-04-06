c81b41e780d00f363b8bc55817a99a6e5401cd45
sasmita sahu <sasmitasahu2119@gmail.com>
2024-04-06 10:44:39 +0530

    Line 6 change

--- src/p1/Test1.java
+++ src/p1/Test1.java
@@ -1,11 +1,8 @@
 package p1;
 
 public class Test1 {
-
 	public static void main(String[] args) {
 		System.out.println("In Test1 file");
-
+		System.out.println("Line Number 6 change n Test1 ");
 	}
-
-
 }
